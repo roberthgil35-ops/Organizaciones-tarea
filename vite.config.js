b89e751c31204467742a5a47fba06215881cbb86
roberthgil35-ops <roberth.gil35@unach.mx>
2026-05-01 21:59:32 -0600

    Corrección de rutas de script

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Esto le dice a Vite que todos los archivos están dentro de la subcarpeta del repo
   base: '/Organizaciones-tarea/', 
 })
